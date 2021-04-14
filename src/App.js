bc1ab08326fc2278817f04304bc34c1d2931a757
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-14 15:22:08 -0400

    diagonal logic

--- src/App.js
+++ src/App.js
@@ -59,7 +59,53 @@ function App() {
       this.setState({ message: 'Game over. Please start a new game.' });
     }
   }
+const checkVertical = (board) => {
+  // checking if rows are 3 and up
+  for (let r=3; r<6; r++){
+    for (let c =0; c<7; c++){
+      if (board[r][c]){
+        if(board[r][c] === board[r - 1][c]  &&
+          board[r][c] === board[r - 2][c] &&
+          board[r][c] === board [r - 3][c]
+          ){
+            return board[r][c]
+          }
+      }
+    }
+  }
 
+}
+const checkHorizontal = (board) =>{
+// check if column 3 of less
+for (let r=0; r<6; r++){
+  for(let c =0; c< 4 ; c++){
+    if (board[r][c]){
+      if (
+        board[r][c] === board[r][c+1] &&
+        board[r][c] === board[r][c+2] &&
+        board[r][c] === board[r][c+3] 
+      ) {
+        return board[r][c]
+      }
+    }
+  }
+}
+}
+
+ const checkDiagonalRight = (board) => {
+  //  Check only if row is 3 or greater AND column is 3 or less
+  for (let r=3; r < 6; r++){
+    for(let c=0; c <4; c++){
+      if (board[r][c]){
+        if (board[r][c] === board[r - 1][c + 1] &&
+          board[r][c] === board[r - 2][c + 2] &&
+          board[r][c] === board[r - 3][c + 3]) {
+        return board[r][c];
+      }
+      }
+    }
+  }
+ }
 
   return (
     <div class="container">

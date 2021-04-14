1e4915bd2b95774dcb285568199ed78a1aca8228
Ashtyn Ce <ashtyncza@gmail.com>
2021-04-14 14:43:58 -0400

    ashie

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
   });
   // bind the play function to game
   game.play = game.play.bind(game);
-  const initBoard = () =>{
+  const initBoard = () => {
     let board = [];
-    for (let r =0; r < 6; r++){
-      let  row =[];
-      for(let c=0; c<7; c++){ row.push(null)}
+    for (let r = 0; r < 6; r++) {
+      let row = [];
+      for (let c = 0; c < 7; c++) { row.push(null) }
       board.push(row)
     }
     this.setState({
@@ -30,8 +30,39 @@ function App() {
       message: ''
     });
   }
+
+  const togglePlayer = () => {
+    return (game.state.currentPlayer === game.state.player1) ? this.state.player2 : this.state.player1;
+  }
+
+  const play = (c) => {
+    if (!game.state.gameOver) {
+      let board = game.state.board;
+      for (let r = 5; r >= 0; r--) {
+        if (!board[r][c]) {
+          board[r][c] = game.state.currentPlayer;
+          break;
+        }
+      }
+
+      let result = game.checkAll(board);
+      if (result === game.state.player1) {
+        game.setState({ board, gameOver: true, message: 'Player 1 (red) wins!' });
+      } else if (result === game.state.player2) {
+        this.setState({ board, gameOver: true, message: 'Player 2 (yellow) wins!' });
+      } else if (result === 'draw') {
+        this.setState({ board, gameOver: true, message: 'Draw game.' });
+      } else {
+        this.setState({ board, currentPlayer: this.togglePlayer() });
+      }
+    } else {
+      this.setState({ message: 'Game over. Please start a new game.' });
+    }
+  }
+
+
   return (
-  <div class="container">
+    <div class="container">
     </div>
   );
 }

14e7765cf6507021388caa3856895cc17785c3ed
rqsell <68514875+rqsell@users.noreply.github.com>
2021-04-14 14:24:19 -0400

    react connect 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 import './App.css';
 
+
 function App() {
+  // Create  state of "game"
+  const [game, setGame] = useState({
+    player1: 1,
+    player2: 2,
+    currentPlayer: null,
+    board: [],
+    gameOver: false,
+    message: ''
+  });
+  // bind the play function to game
+  game.play = game.play.bind(game);
+  const initBoard = () =>{
+    let board = [];
+    for (let r =0; r < 6; r++){
+      let  row =[];
+      for(let c=0; c<7; c++){ row.push(null)}
+      board.push(row)
+    }
+    this.setState({
+      board,
+      currentPlayer: game.state.player1,
+      gameOver: false,
+      message: ''
+    });
+  }
   return (
   <div class="container">
-<div class="currentPlayer">The current player is 
-<span class="player"></span>
-<div class="board"></div>
-<div class="restart">
-<button class="playAgain">play again</button>
-</div>
-</div>
     </div>
   );
 }

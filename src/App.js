541c9162facd61f590257faaa50e1f713dc8ab62
Ashtyn Ce <ashtyncza@gmail.com>
2021-04-20 23:19:20 -0400

    jfhg

--- src/App.js
+++ src/App.js
@@ -154,7 +154,30 @@ function App() {
     )
   }
 
+  const Row = ({ row, play }) => {
+    return (
+      <tr>
+        {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} play={play} />)}
+      </tr>
+    );
+  };
 
+  const Cell = ({ value, columnIndex, play }) => {
+    let color = 'white';
+    if (value === 1) {
+      color = 'red';
+    }
+    else if (value === 2) {
+      color = 'yellow';
+    }
+    return (
+      <td>
+        <div className="cell" onClick={() => { play(columnIndex) }}>
+          <div className={color}></div>
+        </div>
+      </td>
+    );
+  };
 
   return (
     <div class="container">
